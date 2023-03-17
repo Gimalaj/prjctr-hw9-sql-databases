738bc8b4131c77513ea4e9b0772332eb4fb7b99d
Andriyan Koval <Gimalaj@gmail.com>
2023-03-17 19:15:18 +0200

    Add tests

--- nginx/html/index.php
+++ nginx/html/index.php
@@ -9,20 +9,47 @@ $dbname = 'prjctr';
 $conn = new mysqli($host, $user, $pass, $dbname, '3306');
 
 // Check connection
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
-//else {
-//    echo "Connection success";
-//}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else {
+    echo "Connection success \n";
+}
 
-$result = $conn->query("SELECT COUNT(*) FROM users");
-$numRows = $result->fetch_row()[0];
+if (isset($_GET["current_users"])) {
+    $result = $conn->query("SELECT COUNT(*) FROM users");
+    $numRows = $result->fetch_row()[0];
+    echo "Total users $numRows \n";
+}
 
-//$limit = $conn->query("SELECT *FROM users LIMIT 10");
+// Select all users with random birth year. Year should be in range from 1900 to 2022
+if (isset($_GET["rand_year"])) {
+    $rand_year = $_GET["rand_year"];
+    $start_time = microtime(true);
+    $result = $conn->query("SELECT * FROM users WHERE YEAR(birthday) = FLOOR(RAND() * (2023 - 1900)) + 1900;");
+    $end_time = microtime(true);
+    $execution_time = $end_time - $start_time;
+    echo "\n Here is execution time of select users $execution_time that were born in random year";
+}
 
-//echo "First 10 users $limit \n";
+// Select all users that were born in a specific range of years
+if (isset($_GET["range"])) {
+    $range = $_GET["range"];
+    $start_time = microtime(true);
+    $result = $conn->query("SELECT * FROM users WHERE YEAR(birthday) BETWEEN " . $range . ";");
+    $end_time = microtime(true);
+    $execution_time = $end_time - $start_time;
+    echo "\n Here is execution time of select users $execution_time born in a specific range of years";
+}
 
-echo "Total users $numRows \n";
+// Select all users that were born in a random month
+if (isset($_GET["rand_month"])) {
+    $rand_month = $_GET["rand_month"];
+    $start_time = microtime(true);
+    $result = $conn->query("SELECT * FROM users WHERE MONTH(birthday) = FLOOR(RAND() * 12) + 1;");
+    $end_time = microtime(true);
+    $execution_time = $end_time - $start_time;
+    echo "\n Here is execution time of select users $execution_time born in a random month";
+}
 
 ?>
